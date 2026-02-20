55bba33ad98e5514b1111a086e6d0a396f881550
Damian Holona <damianholona074@gmail.com>
2026-02-20 18:48:57 +0100

    + init

---
+++ src/test/java/pl/kurs/github_info/GithubInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kurs.github_info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
