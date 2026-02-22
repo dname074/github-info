3c988b786637a81231bfbe034635e52694d9bf9e
Damian Holona <damianholona074@gmail.com>
2026-02-22 20:41:31 +0100

    [task1] add logs

--- src/main/java/pl/kurs/github_info/controller/GithubInfoController.java
+++ src/main/java/pl/kurs/github_info/controller/GithubInfoController.java
@@ -1,6 +1,7 @@
 package pl.kurs.github_info.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +12,13 @@ import pl.kurs.github_info.service.GithubInfoService;
 @RequestMapping("/repositories")
 @RequiredArgsConstructor
 @RestController
+@Slf4j
 public class GithubInfoController {
     private final GithubInfoService service;
 
     @GetMapping("/{owner}/{repo}")
     public RepoInfoDto getRepoInfoByOwnerAndName(@PathVariable String owner, @PathVariable String repo) {
+        log.info("Received GET /repositories/{}/{} request", owner, repo);
         return service.getRepoInfoByOwnerAndName(owner, repo);
     }
 }
--- src/main/java/pl/kurs/github_info/service/GithubInfoService.java
+++ src/main/java/pl/kurs/github_info/service/GithubInfoService.java
@@ -1,6 +1,7 @@
 package pl.kurs.github_info.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import pl.kurs.github_info.client.GithubClient;
 import pl.kurs.github_info.dto.RepoInfoDto;
@@ -9,12 +10,16 @@ import pl.kurs.github_info.mapper.RepoInfoMapper;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class GithubInfoService {
     private final GithubClient client;
     private final RepoInfoMapper mapper;
 
     public RepoInfoDto getRepoInfoByOwnerAndName(String owner, String repository) {
-        return mapper.toDto(client.getRepoInfoByOwnerAndName(owner, repository)
+        log.info("Process of getting informations about github repository started");
+        RepoInfoDto repoInfoDto = mapper.toDto(client.getRepoInfoByOwnerAndName(owner, repository)
                 .orElseThrow(() -> new RepositoryNotFoundException("Nie znaleziono podanego repozytorium")));
+        log.info("Process of getting informations about github repository ended");
+        return repoInfoDto;
     }
 }
