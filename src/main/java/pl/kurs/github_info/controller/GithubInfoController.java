aafe4b869b6180e49bed10693d935c5ace4d8fd9
Damian Holona <damianholona074@gmail.com>
2026-02-22 20:54:14 +0100

    [task1] add validation annotations and controller docs

--- src/main/java/pl/kurs/github_info/controller/GithubInfoController.java
+++ src/main/java/pl/kurs/github_info/controller/GithubInfoController.java
@@ -1,11 +1,19 @@
 package pl.kurs.github_info.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.kurs.github_info.dto.GithubInfoExceptionDto;
 import pl.kurs.github_info.dto.RepoInfoDto;
 import pl.kurs.github_info.service.GithubInfoService;
 
@@ -13,11 +21,26 @@ import pl.kurs.github_info.service.GithubInfoService;
 @RequiredArgsConstructor
 @RestController
 @Slf4j
+@Tag(name = "Github operations")
 public class GithubInfoController {
     private final GithubInfoService service;
 
+    @Operation(summary = "Get informations about github repository by owner name and repo name")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Repository exists, data returned",
+            content = {
+                    @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = RepoInfoDto.class))
+            }),
+            @ApiResponse(responseCode = "404", description = "Repository not found",
+            content = {
+                    @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = GithubInfoExceptionDto.class))
+            })
+    }
+    )
     @GetMapping("/{owner}/{repo}")
-    public RepoInfoDto getRepoInfoByOwnerAndName(@PathVariable String owner, @PathVariable String repo) {
+    public RepoInfoDto getRepoInfoByOwnerAndName(@PathVariable @NotBlank String owner, @PathVariable @NotBlank String repo) {
         log.info("Received GET /repositories/{}/{} request", owner, repo);
         return service.getRepoInfoByOwnerAndName(owner, repo);
     }
