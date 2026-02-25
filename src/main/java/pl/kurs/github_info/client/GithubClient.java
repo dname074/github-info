a91e0fc1d9862f9e2d4f84cda5bd9c849beea2f0
Damian Holona <damianholona074@gmail.com>
2026-02-25 16:29:10 +0100

    [task3] implement fallback

--- src/main/java/pl/kurs/github_info/client/GithubClient.java
+++ src/main/java/pl/kurs/github_info/client/GithubClient.java
@@ -4,9 +4,14 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import pl.kurs.github_info.client.config.GithubClientConfiguration;
+import pl.kurs.github_info.client.config.GithubClientFallback;
 import pl.kurs.github_info.model.RepoInfo;
 
-@FeignClient(name = "githubClient", url = "https://api.github.com/", configuration = GithubClientConfiguration.class)
+@FeignClient(name = "githubClient",
+        url = "https://api.github.com/",
+        configuration = GithubClientConfiguration.class,
+        fallback = GithubClientFallback.class
+)
 public interface GithubClient {
     @GetMapping("/repos/{owner}/{repo}")
     RepoInfo getRepoInfoByOwnerAndName(@PathVariable("owner") String owner, @PathVariable("repo") String repo);
